20f7b30952d5feeb59222eee09112f46099d84a9
David Banks <amoebae@gmail.com>
2021-12-26 12:13:03 +0000

    integrating code

--- gebunden/views.py
+++ gebunden/views.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 import pyramid.response
-#import gebunden.format_livejournal_archive
+from gebunden.format_livejournal_archive import parse_entries
 
 @view_config(
     route_name='home',
@@ -12,9 +12,24 @@ def my_view(request):
 
 @view_config(
     route_name='entry',
+    renderer='gebunden:templates/entry.j2'
 )
 def entry(request):
     month = request.matchdict['month']
-    index = request.matchdict['index']
+    index = int(request.matchdict['index'])
+
+    path = "{}/{}.xml".format(
+        "/home/amoe/livejournal_amoe_archive",
+        month
+    )
+    
+    with open(path, 'r') as f:
+        entries = parse_entries(f)
+
+    if index > len(entries) - 1:
+        raise Exception('invalid entry index')
+
+    entry = entries[index]
+    
     # ... do some crap here ...
-    return pyramid.response.Response("Hello, world!")
+    return {'event': entry.event, 'month': month, 'index': index}
