fe1be7d2cc5acd76162a6dca39c01c6b9310146f
David Banks <amoebae@gmail.com>
2021-12-26 11:07:39 +0000

    reasonable start

--- gebunden/__init__.py
+++ gebunden/__init__.py
@@ -4,6 +4,7 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
+        config.add_jinja2_renderer('.j2')
 
         config.add_static_view('static', 'static')
         config.add_route('home', '/')
---
+++ gebunden/views/default.py
@@ -0,0 +1,8 @@
+from pyramid.view import view_config
+
+@view_config(
+    route_name='home',
+    renderer='gebunden:templates/home.j2'
+)
+def my_view(request):
+    return {'project': 'myproject'}
